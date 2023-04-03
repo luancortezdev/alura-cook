ec0eab477b9a4298121aa132da63d5882696e025
luancortez <luancortezdev@gmail.com>
2023-04-03 11:44:38 -0300

    feat: :sparkles: Componente IngredienteSelecionado criado e implementado
    Componente especializado

---
+++ src/lib/components/IngredienteSelecionado.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Tag from "./Tag.svelte";
+
+  export let ingrediente: string;
+
+  let selecionado = false;
+
+  function aoClicar() {
+    selecionado = !selecionado;
+  }
+</script>
+
+<button on:click={aoClicar}>
+  <Tag ativa={selecionado}>
+    {ingrediente}
+  </Tag>
+</button>
